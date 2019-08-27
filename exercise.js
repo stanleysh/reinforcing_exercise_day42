4e8cceab54fe9882c99c840d68864a58b649ce98
Stanley Sham <stanley.sham94@gmail.com>
2019-08-27 15:17:03 -0400

    completed assignment

---
+++ exercise.js
@@ -0,0 +1,43 @@
+let project = { 
+    committee: ["Stella", "Salma", "Kai"],
+    title: "Very Important Project",
+    dueDate : "December 14, 2019",
+    id: "3284",
+    steps: [
+      {description: "conduct interviews",
+       dueDate: "August 1, 2018"
+      },
+      {description: "code of conduct",
+       dueDate: "January 1, 2018"
+      },
+      {description: "compile results",
+       dueDate: "November 10, 2018"
+      },
+      {description: "version 1",
+       dueDate: "January 15, 2019"
+      },
+      {description: "revisions",
+       dueDate: "March 30, 2019"
+      },
+      {description: "version 2",
+       dueDate: "July 12, 2019"
+      },
+      {description: "final edit",
+       dueDate: "October 1, 2019"
+      },
+      {description: "final version",
+       dueDate: "November 20, 2019"
+      },
+      {description: "Wrap up",
+       dueDate: "December 1, 2019"
+      }
+    ]
+  }
+
+  project.steps.forEach((e) => {
+    let tempPerson = Math.floor(Math.random()*3);
+    e.person = project.committee[tempPerson];
+  });
+
+//   Checking that person has been added to each project correctly
+  console.log(project)
